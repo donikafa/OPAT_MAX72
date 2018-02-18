c3553c5639de1bdfa08ae1105ada2cca3467d516
donikafa <34938096+donikafa@users.noreply.github.com>
2018-02-18 06:05:00 +0000

    Update Button.cpp

--- Button.cpp
+++ Button.cpp
@@ -1,136 +1,107 @@
-/*----------------------------------------------------------------------*
- * Arduino Button Library v1.0                                          *
- * Jack Christensen May 2011, published Mar 2012                        *
- *                                                                      *
- * Library for reading momentary contact switches like tactile button   *
- * switches. Intended for use in state machine constructs.              *
- * Use the read() function to read all buttons in the main loop,        *
- * which should execute as fast as possible.                            *
- *                                                                      *
- * This work is licensed under the Creative Commons Attribution-        *
- * ShareAlike 3.0 Unported License. To view a copy of this license,     *
- * visit http://creativecommons.org/licenses/by-sa/3.0/ or send a       *
- * letter to Creative Commons, 171 Second Street, Suite 300,            *
- * San Francisco, California, 94105, USA.                               *
- *----------------------------------------------------------------------*/
+/*
+||
+|| @file Button.cpp
+|| @version 1.6
+|| @author Alexander Brevig
+|| @contact alexanderbrevig@gmail.com
+||
+|| @description
+|| | Provide an easy way of making buttons
+|| #
+||
+|| @license
+|| | Copyright (c) 2009 Alexander Brevig. All rights reserved.
+|| | This code is subject to AlphaLicence.txt
+|| | alphabeta.alexanderbrevig.com/AlphaLicense.txt
+|| #
+||
+*/
 
+//include the class definition
 #include "Button.h"
 
-/*----------------------------------------------------------------------*
- * Button(pin, puEnable, invert, dbTime) instantiates a button object.  *
- * pin      Is the Arduino pin the button is connected to.              *
- * puEnable Enables the AVR internal pullup resistor if != 0 (can also  *
- *          use true or false).                                         *
- * invert   If invert == 0, interprets a high state as pressed, low as  *
- *          released. If invert != 0, interprets a high state as        *
- *          released, low as pressed  (can also use true or false).     *
- * dbTime   Is the debounce time in milliseconds.                       *
- *                                                                      *
- * (Note that invert cannot be implied from puEnable since an external  *
- *  pullup could be used.)                                              *
- *----------------------------------------------------------------------*/
-Button::Button(uint8_t pin, uint8_t puEnable, uint8_t invert, uint32_t dbTime)
-{
-    _pin = pin;
-    _puEnable = puEnable;
-    _invert = invert;
-    _dbTime = dbTime;
-    if (_puEnable != 0)
-        pinMode(_pin, INPUT_PULLUP);    //enable pullup resistor
-    else
-        pinMode(_pin, INPUT);
-    _state = digitalRead(_pin);
-    if (_invert != 0) _state = !_state;
-    _time = millis();
-    _lastState = _state;
-    _changed = 0;
-    _lastChange = _time;
+/*
+|| <<constructor>>
+|| @parameter buttonPin sets the pin that this switch is connected to
+|| @parameter buttonMode indicates PULLUP or PULLDOWN resistor
+*/
+Button::Button(uint8_t buttonPin, uint8_t buttonMode){
+	this->pin=buttonPin;
+    pinMode(pin,INPUT);
+	buttonMode==PULLDOWN ? pulldown() : pullup();
+    state = 0;
+    bitWrite(state,CURRENT,!mode);
 }
 
-/*----------------------------------------------------------------------*
- * read() returns the state of the button, 1==pressed, 0==released,     *
- * does debouncing, captures and maintains times, previous states, etc. *
- *----------------------------------------------------------------------*/
-uint8_t Button::read(void)
-{
-    static uint32_t ms;
-    static uint8_t pinVal;
+/*
+|| Set pin HIGH as default
+*/
+void Button::pullup(void){
+	mode=PULLUP;
+	digitalWrite(pin,HIGH);
+}
 
-    ms = millis();
-    pinVal = digitalRead(_pin);
-    if (_invert != 0) pinVal = !pinVal;
-    if (ms - _lastChange < _dbTime) {
-        _time = ms;
-        _changed = 0;
-        return _state;
+/*
+|| Set pin LOW as default
+*/
+void Button::pulldown(void){
+	mode=PULLDOWN;
+	//digitalWrite(pin,LOW);
+}
+
+/*
+|| Return the bitWrite(state,CURRENT, of the switch
+*/
+bool Button::isPressed(void){
+    bitWrite(state,PREVIOUS,bitRead(state,CURRENT));
+    if (digitalRead(pin) == mode){
+        bitWrite(state,CURRENT,false);
+    } else {
+        bitWrite(state,CURRENT,true);
     }
-    else {
-        _lastState = _state;
-        _state = pinVal;
-        _time = ms;
-        if (_state != _lastState)   {
-            _lastChange = ms;
-            _changed = 1;
-        }
-        else {
-            _changed = 0;
-        }
-        return _state;
+    if (bitRead(state,CURRENT) != bitRead(state,PREVIOUS)){
+        bitWrite(state,CHANGED,true);
+    }else{
+        bitWrite(state,CHANGED,false);
+    }
+	return bitRead(state,CURRENT);
+}
+
+/*
+|| Return true if the button has been pressed
+*/
+bool Button::wasPressed(void){
+    if (bitRead(state,CURRENT)){
+        return true;
+    } else {
+        return false;
     }
 }
 
-/*----------------------------------------------------------------------*
- * isPressed() and isReleased() check the button state when it was last *
- * read, and return false (0) or true (!=0) accordingly.                *
- * These functions do not cause the button to be read.                  *
- *----------------------------------------------------------------------*/
-uint8_t Button::isPressed(void)
-{
-    return _state == 0 ? 0 : 1;
+/*
+|| Return true if state has been changed
+*/
+bool Button::stateChanged(void){
+    return bitRead(state,CHANGED);
 }
 
-uint8_t Button::isReleased(void)
-{
-    return _state == 0 ? 1 : 0;
+/*
+|| Return true if the button is pressed, and was not pressed before
+*/
+bool Button::uniquePress(void){
+    return (isPressed() && stateChanged());
 }
 
-/*----------------------------------------------------------------------*
- * wasPressed() and wasReleased() check the button state to see if it   *
- * changed between the last two reads and return false (0) or           *
- * true (!=0) accordingly.                                              *
- * These functions do not cause the button to be read.                  *
- *----------------------------------------------------------------------*/
-uint8_t Button::wasPressed(void)
-{
-    return _state && _changed;
-}
+/*
+|| @changelog
+|| | 2009-05-05 - Alexander Brevig : Added uniquePress()
+|| | 2009-04-24 - Alexander Brevig : Added wasPressed()
+|| | 2009-04-12 - Alexander Brevig : Added constructor
+|| |                                 Shortened logic
+|| | 2009-04-10 - Alexander Brevig : Namechange from Switch
+|| | 2009-04-07 - Alexander Brevig : Altered API
+|| | 2008-10-23 - Alexander Brevig : Initial Release
+|| | 2008-10-22 - Alexander Brevig : Class implemented
+|| # 
+*/
 
-uint8_t Button::wasReleased(void)
-{
-    return !_state && _changed;
-}
-
-/*----------------------------------------------------------------------*
- * pressedFor(ms) and releasedFor(ms) check to see if the button is     *
- * pressed (or released), and has been in that state for the specified  *
- * time in milliseconds. Returns false (0) or true (1) accordingly.     *
- * These functions do not cause the button to be read.                  *
- *----------------------------------------------------------------------*/
-uint8_t Button::pressedFor(uint32_t ms)
-{
-    return (_state == 1 && _time - _lastChange >= ms) ? 1 : 0;
-}
-
-uint8_t Button::releasedFor(uint32_t ms)
-{
-    return (_state == 0 && _time - _lastChange >= ms) ? 1 : 0;
-}
-
-/*----------------------------------------------------------------------*
- * lastChange() returns the time the button last changed state,         *
- * in milliseconds.                                                     *
- *----------------------------------------------------------------------*/
-uint32_t Button::lastChange(void)
-{
-    return _lastChange;
-}
